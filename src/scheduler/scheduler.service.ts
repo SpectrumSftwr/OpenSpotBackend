911121d48bc18b633e49b2adfaf43d7960c2f9f6
Juan Mejia <Jmejiaa1999@gmail.com>
2025-10-15 17:30:42 -0600

    Update Run Cron Times

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -34,7 +34,7 @@ export class SchedulerService {
   /**
    * Method to Run All triggers that need to be run.
    */
-  @Cron(CronExpression.EVERY_5_MINUTES, {
+  @Cron(CronExpression.EVERY_DAY_AT_2PM, {
     name: "run-triggers-cron-job"
   }) 
   async checkTimeTriggers() {
